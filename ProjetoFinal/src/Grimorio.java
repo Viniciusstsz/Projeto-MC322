7c6a6814014c03d792f25ec7a1c37172b5f75f32
Viniciusstsz <127238090+Viniciusstsz@users.noreply.github.com>
2023-06-25 22:55:25 -0300

    Add files via upload

---
+++ ProjetoFinal/src/Grimorio.java
@@ -0,0 +1,7 @@
+public class Grimorio extends Equipamento {
+    
+    public Grimorio(String Grimorio, int 7, int 5, int 100, int 100, int 0, int 1) {
+        super(Grimorio, 7, 5, 100, 100, 0, 1);
+    }
+    
+}
