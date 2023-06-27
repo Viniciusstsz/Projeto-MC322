d2cdbef1c144d3956cd25ae1b1aa75e38a6d2462
Viniciusstsz <127238090+Viniciusstsz@users.noreply.github.com>
2023-06-27 10:09:03 -0300

    Update Mago.java

--- ProjetoFinal/src/Mago.java
+++ ProjetoFinal/src/Mago.java
@@ -1,6 +1,6 @@
 public class Mago extends Personagem {
     
-    //Construtor de Instâncias para a Classe ClientePJ
+    //Alta energia e consumo de de energia nas sua habilidades
     public Mago(String nome, int vidamax, int vida, int energiamax, int energia) {
         super(nome, vidamax, vida, energiamax, energia);
     }
